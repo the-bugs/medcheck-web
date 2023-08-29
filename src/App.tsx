19c603336438489dc53d3fc9f508a6b8e34e51ec
devruso <jamilsonjr@live.com>
2023-08-29 17:58:36 -0300

    create: componente header mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
 import Root from "./pages/Root";
-import Login from "./pages/Login";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />}>
           <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Route>
       </Routes>
     </BrowserRouter>
