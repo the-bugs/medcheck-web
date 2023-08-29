e376de6cbde067ba654ac174a7f31d9c54f15287
devruso <jamilsonjr@live.com>
2023-08-29 18:26:41 -0300

    fix: header mobile nao fecha ao clicar em links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home(){
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen w-10/12">
             <h1 className="text-5xl">Plataforma de exames</h1>        
         </div>
     )
