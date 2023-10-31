76fbe9c7c8874919909c78cf4089033fce1a8f58
devruso <jamilsonjr@live.com>
2023-10-31 15:43:26 -0300

    fix: Corrige footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,5 @@
-export default function Footer(): JSX.Element{
-    return (
-        <footer className="mt-5">
-            <p></p>
-        </footer>
-    )
-
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="relative bottom-0 mt-5 w-screen h-1/5 bg-greenBlue "></footer>
+  );
 }
