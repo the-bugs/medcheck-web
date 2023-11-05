c381a335608f0d9db8e4a02196f2ee8be655d960
Davi Fernandes <83562801+Beatrizdacruz@users.noreply.github.com>
2023-11-05 19:06:38 -0300

    fix: responsive and transform css to tailwind

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,40 +1,58 @@
-import "./login.css";
 import ButtonComponent from "../components/ui/ButtonComponent";
 import { Link } from "react-router-dom";
+import techman from "../assets/imgs/techman.png";
 export default function Login() {
   return (
-    <section id="login">
+    <section id="login" className="flex items-end justify-center lg:gap-[10rem] gap-0 mt-6">
+      <div>
+        <img src={techman} alt="techman" className="lg:flex hidden"/>
+      </div>
       <div className="forms">
-        <h2 className="Login">Login</h2>
+        <div className="mb-[34px]">
+          <h2 className="text-[32px]">Bem vindo (a) de volta!</h2>
+          <p className="text-[16px]">Insira seus dados</p>
+        </div>
         <form action="adefinir" method="post">
-          <label>Nome de Usuário:</label>
-          <input
-            placeholder="Seu usuário"
-            className="input"
-            type="text"
-            id="nome"
-            name="nome"
-            required
-          />
+          <div className="flex flex-col gap-[16px] mb-[32px]">
+            <label className="text-[24px]">Digite seu email</label>
+            <input
+              placeholder="email"
+              className="input h-[30px] border border-[#BBC8D4] p-2" 
+              type="text"
+              id="nome"
+              name="nome"
+              required
+            />
+          </div>
 
-          <label>Senha:</label>
-          <input
-            placeholder="Sua senha"
-            className="input"
-            type="text"
-            id="empresa"
-            name="empresa"
-          />
+          <div className="flex flex-col gap-[16px]">
+            <label className="text-[24px]">Digite sua senha</label>
+            <input
+              placeholder="senha"
+              className="input h-[30px] border border-[#BBC8D4] p-2"
+              type="text"
+              id="empresa"
+              name="empresa"
+            />
+          </div>
           <Link to="../forgetPassword">
-            <p className="forgotPassword">esqueceu sua senha?</p>
+            <p className="text-[16px] mb-[44px] mt-[16px]">Esqueceu sua senha?</p>
           </Link>
 
-          <ButtonComponent
-            className="w-full md:w-40 mt-4"
-            variant={"secondary"}
-            type="submit">
-            Enviar
-          </ButtonComponent>
+          <div className="mb-[2.7rem]">
+            <ButtonComponent
+              className="w-full md:w-40 mt-4"
+              variant={"third"}
+              type="submit">
+              Cadastrar
+            </ButtonComponent>
+            <ButtonComponent
+              className="w-full md:w-40 mt-4"
+              variant={"secondary"}
+              type="submit">
+              Login
+            </ButtonComponent>
+          </div>
         </form>
       </div>
     </section>
