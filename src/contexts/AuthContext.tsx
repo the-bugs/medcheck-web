8970edfafc9fe6fee810d52669ce225b735fee59
devruso <jamilsonjr@live.com>
2023-11-16 21:31:43 -0300

    fix: Corrige requisicao, decoded token, user

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import React, { createContext, useState, ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 type UserType = {
   nome: string;
   email: string;
-  senha: string;
   tipo: string;
+  user_id: number;
 };
 
 type AuthContextType = {
@@ -33,26 +34,24 @@ const AuthContextProvider: React.FC<AuthContextProviderProps> = ({
   const login = async (email: string, senha: string) => {
     const port = "http://localhost:3001/";
     try {
-      const response = await axios.post(
-        `${port}auth/login`,
-        {
-          email,
-          senha,
-        },
-        {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type",
-            Accept: "*/*",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(user);
-      setUser(response.data.user);
+      const response = await axios.post(`${port}auth/login`, {
+        email,
+        senha,
+      });
+      console.log(response);
       // Salva no armazenamento local os dados do usuário.
       // Pensar se mantemos ou não o token.
+      localStorage.setItem("authToken", response.data.access_token);
       localStorage.setItem("userInfo", JSON.stringify(response.data.user));
+      const token = localStorage.getItem("authToken");
+      console.log(token);
+
+      if (token) {
+        const decodedToken = jwtDecode<any>(token);
+        console.log(decodedToken);
+        setUser(decodedToken);
+      }
+
       navigate("/");
     } catch (err) {
       console.log(err);
