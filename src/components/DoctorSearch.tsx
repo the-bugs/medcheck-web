37071f693a57a1bdff605d10064d1542b9b59d1f
devruso <jamilsonjr@live.com>
2023-11-18 11:12:32 -0300

    feat: Add listagem de especialidades

--- src/components/DoctorSearch.tsx
+++ src/components/DoctorSearch.tsx
@@ -1,9 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
 import mobileIMG from "../assets/imgs/mobile_DoctorSearch.png";
 import desktopIMG from "../assets/imgs/desktop_DoctorSearch.png";
-import StyledInput from "./ui/StyledInput";
 import ButtonComponent from "./ui/ButtonComponent";
 
+interface FormData {
+  searchTerm: string;
+}
+
+interface Specialty {
+  id: number;
+  name: string;
+}
+
 export default function DoctorSearch(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const [specialties, setSpecialties] = useState<Specialty[]>([]);
+
+  useEffect(() => {
+    async function getSpecialties(): Promise<void> {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/especialidades`
+        );
+
+        const specialtiesData: Specialty[] = response.data.map((item: any) => ({
+          id: item.id,
+          name: item.nome,
+        }));
+
+        setSpecialties(specialtiesData);
+      } catch (error) {
+        console.error("Erro na solicitação de especialidades:", error);
+      }
+    }
+    getSpecialties();
+  }, []);
+
+  const onSubmit: SubmitHandler<FormData> = async ({ searchTerm }) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/especialidades/`,
+        {
+          params: { searchTerm },
+        }
+      );
+      // Faça algo com a resposta (response.data) aqui
+      console.log(response.data);
+    } catch (error) {
+      // Lide com erros de solicitação aqui
+      console.error("Erro na solicitação:", error);
+    }
+  };
+
   return (
     <div className="relative flex flex-col md:flex-col justify-center h-[500px]">
       <div className="absolute -z-10 inset-0">
@@ -21,7 +76,7 @@ export default function DoctorSearch(): JSX.Element {
         />
       </div>
 
-      <div className="relative md:ml-80 z-10 bg-opacity-50 bg-gray-600 rounded-md max-w-3xl pt-2 p-2 md:p-4 mx-2">
+      <div className="relative md:ml-80 z-10 bg-opacity-50 bg-gray-600 rounded-md max-w-3xl pt-2 p-2 md:p-4">
         <h1 className="text-4xl md:text-5xl font-semibold  text-white mb-4 ">
           Agende agora sua consulta
         </h1>
@@ -30,13 +85,28 @@ export default function DoctorSearch(): JSX.Element {
           profissional que mais se aplica às suas necessidades!
         </p>
       </div>
-      <form className="flex md:ml-80 flex-col w-full  md:flex-row p-2">
-        <StyledInput
-          placeholder="Digite especilidade ou nome do médico"
-          className="w-full h-12 px-2 mt-4 border-2 border-gray-200 focus:border-gray-400  rounded "
-        />
+      <form
+        className="flex md:ml-80 flex-col w-full  md:flex-row p-2"
+        onChange={handleSubmit(onSubmit)}
+      >
+        <select
+          {...register("searchTerm")}
+          className={`w-full h-12 px-2 mt-4 border-2 max-w-xl ${
+            errors.searchTerm ? "border-red-500" : "border-gray-200"
+          } focus:border-gray-400 rounded`}
+        >
+          <option value="" disabled selected>
+            Selecione especialidade desejada
+          </option>
+          {specialties.map((specialty) => (
+            <option key={specialty.id} value={specialty.name}>
+              {specialty.name}
+            </option>
+          ))}
+        </select>
         <ButtonComponent
           variant={"primary"}
+          type="button"
           className="md:w-40 w-full h-12 mt-4 mx-0 md:ml-6"
         >
           Pesquisar
