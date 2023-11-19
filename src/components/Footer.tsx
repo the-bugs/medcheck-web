459dcd58cd2b95fefd4867f06d2fa0ceab2e04af
Beatriz Cruz <83562801+Beatrizdacruz@users.noreply.github.com>
2023-11-19 11:49:16 -0300

    fix: adjust fonts and add copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import logoFinal from "../assets/imgs/MEDICALCHECKLOGO.svg";
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="bg-[#1373b348] flex justify-center py-[5.6rem] gap-10 lg:gap-20 lg:flex-row flex-col items-center lg:items-start">
+    <footer className="bg-[#1373b323] flex justify-center py-[5.6rem] gap-10 lg:gap-20 lg:flex-row flex-col items-center lg:items-start relative">
       <div>
         <Link to="/">
           <img src={logoFinal} alt="header img" width="80px" className="ml-4" />
@@ -15,16 +15,19 @@ export default function Footer(): JSX.Element {
         </div>
         <div className="flex flex-col gap-6 lg:items-start items-center">
           <h2>Empresa</h2>
-          <p>Sobre nós</p>
-          <p>Carreira</p>
+          <p className="text-sm">Sobre nós</p>
+          <p className="text-sm">Carreira</p>
         </div>
         <div className="flex flex-col gap-6 lg:items-start items-center">
           <h2>Contato</h2>
-          <p>contato@medcheck.com</p>
-          <p>(71) 40002-8922</p>
-          <p>Av. Milton Santos, s/nº - Ondina, Salvador, Bahia</p>
+          <p className="text-sm">contato@medcheck.com</p>
+          <p className="text-sm">(71) 40002-8922</p>
+          <p className="text-sm">
+            Av. Milton Santos, s/nº - Ondina, Salvador, Bahia
+          </p>
         </div>
       </div>
+      <p className="absolute bottom-10 lg:left-20 text-center text-xs text-[#2B353EB2]">© Copyright Medical Check Inc.</p>
     </footer>
   );
 }
